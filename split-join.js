a41aebc5f159c5dd4262b123ce865c8a2c4a7539
webhero-nur <nurealom522@gmail..com>
2022-09-17 19:10:48 +0600

    String: split, slice, substring, concat, join

---
+++ split-join.js
@@ -0,0 +1,25 @@
+const lyrics = 'Tumi bondhu kala pakhi, ami jeno ki. bosonto kale tomai bolte pari ni. kala kala, sada sada.';
+const parts = lyrics.split(' '); // spliting every words using <space>
+// console.log(parts);
+const sentences = lyrics.split('.'); // spliting every sentences using full stop (.)
+// console.log(sentences);
+const chars = lyrics.split(''); // spliting every character using empty string
+// console.log(chars);
+
+const partial = lyrics.slice(5, 11); // slicing a string using index
+// console.log(partial);
+
+const partial2 = lyrics.substring(12, 16); // making a sub string using index. almost same as slicing
+// console.log(partial2);
+
+const partialConcat = partial.concat(' ', partial2);
+console.log(partialConcat);
+
+const lines = [
+    'Tumi bondhu kala pakhi, ami jeno ki',
+    'Bosonto kale tomai bolte pari ni',
+    'Kala kala, sada sada'
+]
+
+const newSong = lines.join('. ');
+console.log(newSong);
