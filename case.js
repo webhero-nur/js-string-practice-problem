7cbe88e3056261d6a478d0ba46328fb85fedd2c2
webhero-nur <nurealom522@gmail..com>
2022-09-17 18:30:43 +0600

    case sensitivity remove

---
+++ case.js
@@ -0,0 +1,10 @@
+const userName = 'NureAlom';
+const inputName = 'nureAlom';
+console.log(userName.toLowerCase());
+console.log(inputName.toLowerCase());
+if (userName.toUpperCase() === inputName.toUpperCase()) {
+    console.log('valid user');
+}
+else {
+    console.log('invalid user');
+}
