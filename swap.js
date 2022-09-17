35e5932504c956fdbcad6f5d533e778ccebca6d8
webhero-nur <nurealom522@gmail..com>
2022-09-17 19:51:30 +0600

    Swap variable, temp variable, without temp vaiable, destructring

---
+++ swap.js
@@ -0,0 +1,15 @@
+let first = 5;
+let second = 9;
+console.log(first, second);
+/* Wrong Approach */
+// first = second;
+// second = first;
+
+/* Approach: 1 (using a temporary variable) */
+// const temp = first;
+// first = second;
+// second = temp;
+
+/* Approach: 2 (Destructuring) */
+[first, second] = [second, first]
+console.log(first, second);
